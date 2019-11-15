0e565bb7b683a314544b157f1005482a6766415f
Amrut <XKS9@eurofins.com>
2019-11-15 15:15:40 +0530

    Removing eureka

--- src/main/java/com/dashlines/DashlinesNewsApp.java
+++ src/main/java/com/dashlines/DashlinesNewsApp.java
@@ -2,15 +2,11 @@ package com.dashlines;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableEurekaClient
-@EnableDiscoveryClient
 @SpringBootApplication
 public class DashlinesNewsApp {
 
