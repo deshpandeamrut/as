997339e5d649f932db99309e825c052ec989a26a
Amrut <XKS9@eurofins.com>
2020-04-29 12:48:48 +0530

    Adding Swagger

--- src/main/java/com/dashlines/controller/NewsService.java
+++ src/main/java/com/dashlines/controller/NewsService.java
@@ -13,24 +13,31 @@ import com.dashlines.entity.Article;
 import com.dashlines.entity.Source;
 import com.dashlines.service.NewsServiceImpl;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "NEWS API")
 @RestController
 public class NewsService {
 
 	@Autowired
 	NewsServiceImpl newsServiceImpl;
 
+	@ApiOperation(value = "Retrieves latest news articles from poular sources")
 	@RequestMapping(value = "/news/getLatest")
 	@ResponseBody
 	public List<Article> getLatestNews() {
 		return newsServiceImpl.getLatestNews();
 	}
 
+	@ApiOperation(value = "Retrieves latest news articles from the specified source")
 	@RequestMapping("/news/source/{sourceName}")
 	@ResponseBody
 	public List<Article> getNewsFromSource(@PathVariable String sourceName) {
 		return newsServiceImpl.getNewsFromSource(sourceName);
 	}
 
+	@ApiOperation(value = "Retrieves latest news articles for the specifies keyword")
 	@RequestMapping(value = "/news/search/{searchText}")
 	@ResponseBody
 	public List<Article> getLatestNewsForSearchText(@PathVariable String searchText) {
@@ -41,36 +48,42 @@ public class NewsService {
 		return temp;
 	}
 
+	@ApiOperation(value = "Retrieves news articles from the specified source")
 	@RequestMapping(value = "/news/all/search/{searchText}")
 	@ResponseBody
 	public List<Article> getAllNewsForSearchText(@PathVariable String searchText) {
 		return newsServiceImpl.getAllNewsForSearchText(searchText);
 	}
 
+	@ApiOperation(value = "Retrieves latest news articles from the specified category")
 	@RequestMapping(value = "/news/category/{category}")
 	@ResponseBody
 	public List<Article> getNewsForCategory(@PathVariable String category, boolean extendedSearch) {
 		return newsServiceImpl.getNewsForCategory(category, extendedSearch);
 	}
 
+	@ApiOperation(value = "Retrieves personilized news articles")
 	@RequestMapping(value = "/news/myFeed")
 	@ResponseBody
 	public Map<String, List<Article>> getLatestFeed() {
 		return newsServiceImpl.getLatestFeed();
 	}
 
+	@ApiOperation(value = "Retrieves available news sources")
 	@RequestMapping(value = "/news/sources")
 	@ResponseBody
 	public List<Source> getNewsSources() {
 		return newsServiceImpl.getNewsSources();
 	}
 
+	@ApiOperation(value = "Retrieves smart feed")
 	@RequestMapping(value = "/news/smartFeed")
 	@ResponseBody
 	public List<Article> getSmartNews() {
 		return newsServiceImpl.getSmartNews("");
 	}
 
+	@ApiOperation(value = "Retrieves latest news articles from the user interested sources")
 	@RequestMapping(value = "/news/mySources")
 	@ResponseBody
 	public Map<String, List<Article>> getMySourcesNews() {
--- src/main/java/com/dashlines/web/gateway/filter/SimpleCORSFilter.java
+++ src/main/java/com/dashlines/web/gateway/filter/SimpleCORSFilter.java
@@ -2,6 +2,7 @@ package com.dashlines.web.gateway.filter;
 
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -10,6 +11,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
